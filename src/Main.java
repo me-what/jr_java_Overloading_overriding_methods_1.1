87362e7e8eaa9f551e5b56e1ab167212065e59b9
withoutLastNameAndFirstName <eldar.979751@gmail.com>
2024-02-04 00:31:51 +0300

    10

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        System.out.println(calculator.sum(5,5)); // 10
-        System.out.println(calculator.sum(5,5,5)); // 15
-        System.out.println(calculator.sum(5.0, 5.0)); // 10.0
+        System.out.println(calculator.sum(5,5));
+        System.out.println(calculator.sum(5,5,5));
+        System.out.println(calculator.sum(5.0, 5.0));
         calculator.sum();
         System.out.println("\n");
 
